972c91d987d948ea9ad9b70792a305a92859921b
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2018-04-10 14:10:33 -0400

    alternate rain water solution using constant space

--- Javascript/042_trappingRainWater.js
+++ Javascript/042_trappingRainWater.js
@@ -26,3 +26,28 @@ var trap = function(heights) {
 
  return totalArea;
 };
+
+// Alternate solution: O(n) time | O(1) space
+function waterArea(heights) {
+  let left = 0
+  let right = heights.length - 1
+  let leftMax = 0
+  let rightMax = 0
+  let water = 0
+
+  while (left < right) {
+    if (heights[left] < heights[right]) {
+      heights[left] >= leftMax
+        ? (leftMax = heights[left])
+        : (water += leftMax - heights[left])
+      left++
+    } else {
+      heights[right] >= rightMax
+        ? (rightMax = heights[right])
+        : (water += rightMax - heights[right])
+      right--
+    }
+  }
+
+  return water
+}
